1f7f5e83016753a4e379e75fc2062263c4c1b45c
GuiEspimFiap <rm99499@gmail.com>
2023-04-25 11:43:01 -0300

    js

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -1,20 +1,54 @@
-const liElement = document.getElementsByTagName('li');
+let bulbStage =  "off";
 
-// for (let index = 0; index < liElement.length; index++) {
-//     console.log(liElement[index].textContent)
-// }
+function changeColor() {
+    let r = "";
+    let g = "";
+    let b = "";
 
-// Declaração de arrays
-let nr1 = [1, 2, 3, 4, 5];
-let nr2 = [6, 7, 8, 9,10];
-// let nr3 = nr1+nr2;
-let nr3 = [nr1, nr2];
+    // Random = Gera números aleatórios entre 0 e 1
+    // ceil, floor e round que arredondam o número para cima, para baixo, aou aleatóriamente
 
-console.log(`Array-1 ${nr1}`);
-console.log(`Array-2 ${nr2}`);
-console.log(`Array-3 ${nr3}`);
+    // Determinando para a variável r um valor entre 0 e 255.
+    r = Math.round(Math.random() * 255);
+    g = Math.round(Math.random() * 255);
+    b = Math.round(Math.random() * 255);
+    
+    const cabecalho = document.querySelector(".cabecalho");
+    cabecalho.setAttribute("style", `background-color: rgb(${r}, ${g}, ${b});`);
+    tmp = setTimeout(changeColor, 1000);
 
-// Listando um array! Técnica especial
-nr3.forEach((item) => {
-    console.log(`Array-3 sendo listado ${item}`);
-})
+    changeImage();
+    
+}
+
+function changeImage() {
+    const le = document.querySelector(".l-e");
+    const ld = document.querySelector(".l-d");
+
+    setTimeout(() => {
+        changeBackground(le)
+        changeBackground(ld)
+    }, 2000);
+} 
+
+function changeBackground(value) {
+    let image = Math.ceil(Math.random() * 3);
+    value.style.backgroundImage = `url(./img/banner-lateral-${image}.png)`;
+}
+
+function turnBulb() {
+    const bulb = document.querySelector(".bulb");
+    const button = document.querySelector(".turnonoff");
+
+    if (bulbStage === "off") {
+        button.innerHTML = "Deligar";
+        bulbStage = "on";
+    } else {
+        button.innerHTML = "Ligar";
+        bulbStage = "off";
+    }
+    bulb.removeAttribute("src");
+    bulb.setAttribute("src",  `./img/pic_bulb${bulbStage}.gif`);
+}
+
+changeColor();
